ce61c04a8799e6827aec299be232c44290a214e0
Yash Shukla <122151963+yawwsh@users.noreply.github.com>
2023-07-15 18:33:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,31 @@
-import streamlit as st
-import pandas as pd
-from statsmodels.tsa.arima.model import ARIMA
-
-@st.cache
-def load_data():
-    data_url = 'https://drive.google.com/file/d/1JE9nCnPrrS7Y4jTwBov7G0DqGJkk-Go9/view?usp=sharing'
-    file_id = data_url.split('/')[-2]
-    csv_url = f'https://drive.google.com/uc?id={file_id}'
-    dataset = pd.read_csv(csv_url)
-    return dataset
-
-def run_arima(dataset, p, d, q):
-    # Splitting into train and test
-    to_row = int(len(dataset) * 0.9)
-    training = list(dataset[0:to_row]['Adj Close'])
-    testing = list(dataset[to_row:]['Adj Close'])
-
-    # Making model
-    model_predictions = []
-    n_test_obser = len(testing)
-
-    for i in range(n_test_obser):
-        model = ARIMA(training, order=(p, d, q))
-        model_fit = model.fit()
-        output = model_fit.forecast()
-        yhat = output[0]
-        model_predictions.append(yhat)
-        actual_test_value = testing[i]
-        training.append(actual_test_value)
-
 def main():
     st.title("ARIMA Prediction")
 
-    dataset = load_data()
+    dataset = load_dataset()
+    df = preprocess_data(dataset)
 
+    st.write("User Input:")
     p = st.text_input("AR Order (p)", "4")
     d = st.text_input("Difference Order (d)", "1")
     q = st.text_input("MA Order (q)", "0")
 
     if st.button("Run ARIMA"):
-        p = int(p)
-        d = int(d)
-        q = int(q)
-        run_arima(dataset, p, d, q)
+        try:
+            p = int(p)
+            d = int(d)
+            q = int(q)
+
+            st.write("Predicted Results:")
+            model_predictions = run_arima(df, p, d, q)
+            st.write(f"Prediction 1: {model_predictions[0]}")
+
+            if model_predictions[0] > df[-1]:
+                st.write("Recommendation: Buy")
+            else:
+                st.write("Recommendation: Sell")
+
+        except ValueError:
+            st.write("Invalid input. Please enter integer values for AR Order, Difference Order, and MA Order.")
 
 if __name__ == '__main__':
     main()
