45e09f996bf12cb0a8847843e38dff886ad540f9
Yash Shukla <122151963+yawwsh@users.noreply.github.com>
2023-07-15 18:35:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,47 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima.model import ARIMA
+
+@st.cache
+def load_dataset():
+    dataset = pd.read_csv("data.csv")
+    return dataset
+
+def preprocess_data(dataset):
+    # Perform any preprocessing steps here
+    # For example, converting the date column to datetime format, sorting the data, etc.
+    df = dataset.copy()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values('Date')
+    return df
+
+def run_arima(dataset, p, d, q):
+    # Preprocess the dataset if required
+    df = preprocess_data(dataset)
+
+    # Splitting into train and test
+    to_row = int(len(df) * 0.9)
+    training = list(df[0:to_row]['Adj Close'])
+    testing = list(df[to_row:]['Adj Close'])
+
+    # Making model
+    model_predictions = []
+    n_test_obser = len(testing)
+
+    for i in range(n_test_obser):
+        model = ARIMA(training, order=(p, d, q))
+        model_fit = model.fit()
+        output = model_fit.forecast()
+        yhat = output[0]
+        model_predictions.append(yhat)
+        actual_test_value = testing[i]
+        training.append(actual_test_value)
+
+    return model_predictions
+
 def main():
-    st.title("ARIMA Prediction")
+    st.title("BTC Prediction Bot")
 
     dataset = load_dataset()
     df = preprocess_data(dataset)
